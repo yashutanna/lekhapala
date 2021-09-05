4ebb99dc3cf3c2bbd38c21bf338fb7868d760f7c
Yashu <yashu.tanna@outlook.com>
2021-09-05 06:48:43 +0200

    disable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3001;
-  app.enableCors({
-    origin: "http://raspberrypi.local:3000/"
-  });
   await app.listen(port);
 }
 bootstrap();
