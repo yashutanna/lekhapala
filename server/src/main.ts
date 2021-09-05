75ec6219cd3d9572ef9954d52913fd1014fbd0d0
Yashu <yashu.tanna@outlook.com>
2021-09-05 07:04:24 +0200

    enable cors for raspberry pi and localhost

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3001;
+  app.enableCors({
+    origin: ["http://raspberrypi.local:3000", "http://localhost:3000"]
+  });
   await app.listen(port);
 }
 bootstrap();
